fa99b82ae8611798f820c6c0714e3f29d07f8136
Bruno Ivan Silva Narro <brunosilva.narro@gmail.com>
2018-10-26 00:08:43 +0200

    hola

--- Acceso-a-BBDD/CrearCarpetasArchivos/src/Carpeta.java
+++ Acceso-a-BBDD/CrearCarpetasArchivos/src/Carpeta.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.util.Scanner;
 
 public class Carpeta {
+    String ruta;
+
+    public Carpeta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public void preguntarNombre(){
+        File carpeta[] = new File[50];
+        String arr[] = ruta.split("/");
+        String nombrePadre = "";
+        for (int i = 0; i < arr.length; i++) {
+            if(nombrePadre.equals(""))
+                carpeta[i] = new File(arr[i]);
+            else
+                carpeta[i] = new File(nombrePadre,arr[i]);
+            nombrePadre = carpeta[i].getPath();
+            carpeta[i].mkdir();
+        }
+    }
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
 }
